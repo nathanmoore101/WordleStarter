75724f1bf028d7a31f5617b9ebf505d5bec26465
Suzi1i1i <105808052+Suzi1i1i@users.noreply.github.com>
2024-01-23 13:30:34 -0700

    milestone3-1

---
+++ .history/Wordle_20240122145515.py
@@ -0,0 +1,56 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+# test suzi
+
+def wordle():
+
+
+    def enter_action(s):
+        l1 = gw.get_square_letter(N_ROWS -6, N_COLS - 5)
+        l2 = gw.get_square_letter(N_ROWS -6, N_COLS - 4)
+        l3 = gw.get_square_letter(N_ROWS -6, N_COLS - 3)
+        l4 = gw.get_square_letter(N_ROWS -6, N_COLS - 2)
+        l5 = gw.get_square_letter(N_ROWS -6, N_COLS - 1)
+        user_word = (l1 + l2 + l3 + l4 + l5)
+
+        if user_word.lower() not in FIVE_LETTER_WORDS:
+            gw.show_message("Not in word list")
+        else:
+            gw.show_message("Valid Word")
+            for o in Outers:
+                for i in Inners
+                if li == random_word[i]:
+                    l1 = 
+                
+            
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+    random_word = random.choice(FIVE_LETTER_WORDS)
+    
+    characters_array = list(random_word)
+
+    for col, letter in enumerate(random_word):
+        gw.set_square_letter(N_ROWS - 6, N_COLS - 5 + col, letter[0])
+
+        
+    
+
+    print("Random word:", random_word)
+
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
---
+++ .history/Wordle_20240123124843.py
@@ -0,0 +1,67 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR, KEY_COLOR 
+
+# test suzi
+
+def wordle():
+    
+    def enter_action(s):
+
+        def compare_guess(row, target, guess):
+            for i in range(0,5):
+                if target[i] == guess[i]:
+                    gw.set_square_color(row, i, CORRECT_COLOR)
+                elif guess[i] in target:
+                    if target.count(guess[i]) > guess[:i].count(guess[i]):
+                        gw.set_square_color(row, i, PRESENT_COLOR)
+                    else:
+                        gw.set_square_color(row, i, MISSING_COLOR)
+                else:
+                    gw.set_square_color(row, i, MISSING_COLOR) 
+
+        l1 = gw.get_square_letter(N_ROWS -6, N_COLS - 5)
+        l2 = gw.get_square_letter(N_ROWS -6, N_COLS - 4)
+        l3 = gw.get_square_letter(N_ROWS -6, N_COLS - 3)
+        l4 = gw.get_square_letter(N_ROWS -6, N_COLS - 2)
+        l5 = gw.get_square_letter(N_ROWS -6, N_COLS - 1)
+        user_word = (l1 + l2 + l3 + l4 + l5)
+        user_array = list(user_word)
+        
+        if user_word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("Valid Word")
+            compare_guess(0,rand_array,user_array)
+            if rand
+        else:
+            gw.show_message("Not in word list")
+            
+    random_word = random.choice(FIVE_LETTER_WORDS)
+    rand_array = list(random_word.upper())
+    
+    print("Random word:", random_word)
+
+    
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+        
+
+         
+    #for col, letter in enumerate(random_word):
+        #gw.set_square_letter(N_ROWS - 6, N_COLS - 5 + col, letter[0])
+        
+        
+    
+
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
