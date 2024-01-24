cd09f708abf13b3e13fbeaea1afe5fb63e12ee67
nathanmoore101 <103144908+nathanmoore101@users.noreply.github.com>
2024-01-24 01:00:03 -0700

    MIlestone 3 pt 2 + enter/delete functionality

--- WordleGraphics.py
+++ WordleGraphics.py
@@ -99,26 +99,55 @@ class WordleGWindow:
             if isinstance(tke, str):
                 ch = tke.upper()
             else:
-                ch = tke.char.upper()
-            if ch == "\007" or ch == "\177" or ch == "DELETE":
+                ch = tke.keysym.upper()
+
+            # Handle "BACKSPACE" or "DELETE" for both physical and onscreen keyboards
+            if ch == "BACKSPACE" or ch == "DELETE":
                 self.show_message("")
                 if self._row < N_ROWS and self._col > 0:
                     self._col -= 1
                     sq = self._grid[self._row][self._col]
                     sq.set_letter(" ")
-            elif ch == "\r" or ch == "\n" or ch == "ENTER":
+            
+            # Handle "RETURN" or "ENTER" for both physical and onscreen keyboards
+            elif ch == "RETURN" or ch == "ENTER":
                 self.show_message("")
                 s = ""
                 for col in range(N_COLS):
-                    s += self._grid[self._row][col].get_letter();
+                    s += self._grid[self._row][col].get_letter()
                 for fn in self._enter_listeners:
                     fn(s)
+
+            # Handle other alphabetical keys for both physical and onscreen keyboards
             elif ch.isalpha():
                 self.show_message("")
                 if self._row < N_ROWS and self._col < N_COLS:
                     sq = self._grid[self._row][self._col]
                     sq.set_letter(ch)
                     self._col += 1
+            # if isinstance(tke, str):
+            #     ch = tke.upper()
+            # else:
+            #     ch = tke.char.upper()
+            # if ch == "\007" or ch == "\177" or ch == "DELETE":
+            #     self.show_message("")
+            #     if self._row < N_ROWS and self._col > 0:
+            #         self._col -= 1
+            #         sq = self._grid[self._row][self._col]
+            #         sq.set_letter(" ")
+            # elif ch == "\r" or ch == "\n" or ch == "ENTER":
+            #     self.show_message("")
+            #     s = ""
+            #     for col in range(N_COLS):
+            #         s += self._grid[self._row][col].get_letter();
+            #     for fn in self._enter_listeners:
+            #         fn(s)
+            # elif ch.isalpha():
+            #     self.show_message("")
+            #     if self._row < N_ROWS and self._col < N_COLS:
+            #         sq = self._grid[self._row][self._col]
+            #         sq.set_letter(ch)
+            #         self._col += 1
 
         def press_action(tke):
             self._down_x = tke.x
