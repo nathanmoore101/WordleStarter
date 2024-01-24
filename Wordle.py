f4995ace476a00d96114cf3179307791a964b6de
nathanmoore101 <103144908+nathanmoore101@users.noreply.github.com>
2024-01-23 23:44:32 -0700

    milestone 3 part 1

--- Wordle.py
+++ Wordle.py
@@ -8,36 +8,56 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR, KEY_COLOR 
 
+# milestone 3
 
 def wordle():
-
-
+    
     def enter_action(s):
+
+        def compare_guess(row, target, guess):
+            for i in range(0,5):
+                if target[i] == guess[i]:
+                    gw.set_square_color(row, i, CORRECT_COLOR)
+                elif guess[i] in target:
+                    if target.count(guess[i]) > guess[:i].count(guess[i]):
+                        gw.set_square_color(row, i, PRESENT_COLOR)
+                    else:
+                        gw.set_square_color(row, i, MISSING_COLOR)
+                else:
+                    gw.set_square_color(row, i, MISSING_COLOR) 
+
         l1 = gw.get_square_letter(N_ROWS -6, N_COLS - 5)
         l2 = gw.get_square_letter(N_ROWS -6, N_COLS - 4)
         l3 = gw.get_square_letter(N_ROWS -6, N_COLS - 3)
         l4 = gw.get_square_letter(N_ROWS -6, N_COLS - 2)
         l5 = gw.get_square_letter(N_ROWS -6, N_COLS - 1)
         user_word = (l1 + l2 + l3 + l4 + l5)
-
-        if user_word.lower() not in FIVE_LETTER_WORDS:
-            gw.show_message("Not in word list")
-        else:
+        user_array = list(user_word)
+        
+        if user_word.lower() in FIVE_LETTER_WORDS:
             gw.show_message("Valid Word")
-
-    gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-
+            compare_guess(0,rand_array,user_array)
+        else:
+            gw.show_message("Not in word list")
+            
     random_word = random.choice(FIVE_LETTER_WORDS)
-
-    for col, letter in enumerate(random_word):
-        gw.set_square_letter(N_ROWS - 6, N_COLS - 5 + col, letter[0])
+    rand_array = list(random_word.upper())
+    
+    print("Random word:", random_word)
 
     
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+        
 
-    print("Random word:", random_word)
+         
+    for col, letter in enumerate(random_word):
+        gw.set_square_letter(N_ROWS - 6, N_COLS - 5 + col, letter[0])
+        
+        
+    
 
 
 # Startup code
